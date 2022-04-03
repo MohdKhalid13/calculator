e77fd7cb662f07f95740816098d9cee0e0ed0c26
MohdKhalid13 <mohdkhalid.mansoori@gmail.com>
2022-04-03 20:22:18 +0530

    calculator

---
+++ sat.js
@@ -0,0 +1,23 @@
+function myfunction(val){
+
+    document.getElementById('output').value += val
+
+    return val
+
+}
+function solve(){
+
+    let x = document.getElementById('output').value
+
+    let y = eval(x);
+
+    document.getElementById('output').value = y
+
+    return y
+
+}
+function clearScreen(){
+
+    document.getElementById('output').value = ''
+
+}
